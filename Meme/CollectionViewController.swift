9f4d066e32483218f32f1a70d3cd0eddb26d10ce
Martin Kelly <martinkelly@Martins-MacBook-Pro.local>
2015-08-30 18:53:38 +0100

    started to setup layout for v2, added tab bar controller and table view

---
+++ Meme/CollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CollectionViewController.swift
+//  Meme
+//
+//  Created by Martin Kelly on 30/08/2015.
+//  Copyright (c) 2015 Scriptable. All rights reserved.
+//
+
+import Foundation
---
+++ Meme/TableVIewController.swift
@@ -0,0 +1,20 @@
+//
+//  TableVIewController.swift
+//  Meme
+//
+//  Created by Martin Kelly on 30/08/2015.
+//  Copyright (c) 2015 Scriptable. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewController: UITableViewController {
+    var memes:[Meme] = []
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+}
